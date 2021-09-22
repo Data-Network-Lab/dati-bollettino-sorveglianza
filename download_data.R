21d47061bbf8f48cc4ad683523be7a03612fb2d9
Niccolo Salvini <niccolo.salvini27@gmail.com>
2021-09-22 16:29:08 +0200

    moved download_darta out dir directly in `data/*`

--- download_data.R
+++ download_data.R
@@ -7,13 +7,10 @@
 zip_folder <- "https://www.epicentro.iss.it/coronavirus/open-data/OPENDATA-2021.zip"
 
 temp <- tempfile()
-download.file(scrape_url, temp)
-unzip(temp, exdir = here("data", "data-raw-prova"))
+download.file(zip_folder, temp)
+unzip(temp, exdir = here("data"))
 unlink(temp)
 
-
-## unzip ito folder
-unzip(zipfile = scrape_url, exdir = here("data", "data-raw-prova"))
 
 ## TODO:
 ## - togliere la cartella OPENDATA (muovere 1 level upper)
