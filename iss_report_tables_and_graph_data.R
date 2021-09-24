bb18cc4f64964ae82f4d85727f3c747f3d0f54e1
Niccolo Salvini <niccolo.salvini27@gmail.com>
2021-09-24 18:48:49 +0200

    implemented grouping by time to sesso_eta_data

--- iss_report_tables_and_graph_data.R
+++ iss_report_tables_and_graph_data.R
@@ -5,20 +5,48 @@ library(gt, quietly = T, warn.conflicts = F)
 library(timetk, quietly = T, warn.conflicts = F)
 
 ## per mese
+
+sesso_eta = readRDS(here("data", "sesso_eta_data", "data.Rds"))
 sesso_eta %>%
   clean_names() %>% 
-  mutate(iss_date  = dmy(iss_date)) %>% 
-  filter_by_time(.date_var = iss_date, .start_date = dmy("01/01/2021"), .end_date = today()) %>% 
-  summarise_by_time(.date_var = iss_date, 
-                    .by = "month",
-                    casi_cumulativi = sum(casi_cumulativi))
-  group_by(age_group) %>% 
-  summarise(wh)
+  mutate(
+    iss_date  = dmy(iss_date),
+    deceduti = ifelse(deceduti == "<5", 2, deceduti),
+    casi_cumulativi = ifelse(casi_cumulativi == "<5", 2, casi_cumulativi),
+    mutate(across(c("deceduti", "casi_cumulativi"), as.numeric))
+         ) %>%
+  group_by(iss_date, sesso, age_group) %>% 
+  summarise_by_time(
+    .date_var = iss_date, 
+    .by       = "month",
+    casi_cumulativi  = last(casi_cumulativi),
+    deceduti         = sum(deceduti)
+  ) %>%
+  filter_by_time(
+    .date_var = iss_date,
+    .start_date = dmy("01/01/2021"),
+    .end_date = today()
+  ) 
 
 
 ## per settimana
+## assumption: quando è minor di 5 (.e. "<5") metto 2.
 sesso_eta %>%
   clean_names() %>%
-  mutate(iss_date  = dmy(iss_date)) %>% 
-  filter_by_time(.date_var = iss_date, .start_date = dmy("01/01/2021"), .end_date = today()) %>%
-  summarise_by_time(.date_var = iss_date, .by = "week")
+  mutate(iss_date  = dmy(iss_date),
+         deceduti = ifelse(deceduti == "<5", 2, deceduti),
+         casi_cumulativi = ifelse(casi_cumulativi == "<5", 2, casi_cumulativi),
+         mutate(across(c("deceduti", "casi_cumulativi"), as.numeric))
+         )  %>% 
+  group_by(iss_date, sesso, age_group) %>% 
+  summarise_by_time(
+    .date_var = iss_date, 
+    .by       = "week",
+    casi_cumulativi  = last(casi_cumulativi),
+    deceduti         = sum(deceduti)
+    ) %>%
+  filter_by_time(
+      .date_var = iss_date,
+      .start_date = dmy("01/01/2021"),
+      .end_date = today()
+    ) 
